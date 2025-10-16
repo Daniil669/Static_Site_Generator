5d51948f7d7382c97778edae9d9c450b7ea2f291
Daniil669 <daniil.freeman669@gmail.com>
2025-10-16 15:44:20 +0300

    TextNode and inital setup

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+def main():
+    txt_node = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print(txt_node)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN_TEXT = "plain"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK_TEXT = "link text"
+    LINK_IMG = "link img"
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, text_node_1, text_node_2):
+        if text_node_1.text == text_node_2.text and text_node_1.text_type == text_node_2.text_type and text_node_1.url == text_node_2.url:
+            return True
+        return False
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
